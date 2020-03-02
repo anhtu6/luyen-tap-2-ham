67a98f7d8abda6bb4aa64d89ecacdf1175de1ce3
anhtu6 <anhtu632@gmail.com>
2020-03-02 11:39:32 +0700

    updated

--- js/check-is-number.js
+++ js/check-is-number.js
@@ -1,10 +1,10 @@
-let n= prompt('input');
-let array1 =n.split('');
+let string= prompt('input');
+let array1 =string.split('');
 function check(array) {
 
     for (let i=0;i<array.length;i++){
         if (isNaN(parseInt(array[i]))) return false;
     }
 }
-if (check(array1)==false) {alert('not a')}
-else {alert('la so')};
+if (check(array1)==false) {alert('not a number')}
+else {alert('is a number')};
